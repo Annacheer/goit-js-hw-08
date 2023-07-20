bc030400bcc084d1ca852474e0d278cd50fe4cdd
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-20 17:27:24 -0400

    Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageTextarea.value = formState.message;
+  }
+};
+
+form.addEventListener('input', () => {
+  saveFormState();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(formState);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
 
