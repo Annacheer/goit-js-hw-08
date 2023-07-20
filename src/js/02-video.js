5efd772eea5e28728419fca97c0f065f27362d69
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-20 15:41:48 -0400

    Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+const updateCurrentTime = (time) => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+const getCurrentTime = () => {
+  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+};
+
+player.ready().then(() => {
+  player.getCurrentTime().then((time) => {
+    updateCurrentTime(time);
+  });
+
+  player.on('timeupdate', throttle((event) => {
+    updateCurrentTime(event.seconds);
+  }, 1000));
+});
+
+window.addEventListener('load', () => {
+  const currentTime = getCurrentTime();
+  player.setCurrentTime(currentTime).then(() => {
+    player.play();
+  });
+});
+
