61dc669c09e07de3f40b2debbc6d47686738b443
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-20 11:21:50 -0400

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+const imagesListArr = galleryItems.map((image) => {
+  return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`;
+});
+
+const listTemplate = imagesListArr.join('');
+galleryList.insertAdjacentHTML('beforeend', listTemplate);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: (el) => el.dataset.alt,
+});
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+  lightbox.open({ content: `<img src="${largeImageUrl}" alt="${event.target.alt}">` });
+});
+
+
 
 console.log(galleryItems);
