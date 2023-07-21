d072011f61948caa28b360d3b6799aaed56cd946
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-21 10:21:18 -0400

    Task-2 - fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,36 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player');
-
-const updateCurrentTime = (time) => {
-  localStorage.setItem('videoplayer-current-time', time);
-};
-
-const getCurrentTime = () => {
-  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
-};
-
-player.ready().then(() => {
-  player.getCurrentTime().then((time) => {
-    updateCurrentTime(time);
-  });
-
-  player.on('timeupdate', throttle((event) => {
-    updateCurrentTime(event.seconds);
-  }, 1000));
+const player = new Player('vimeo-player', {
+  id: "vimeo-player",
+  width: 640
 });
 
-window.addEventListener('load', () => {
-  const currentTime = getCurrentTime();
-  player.setCurrentTime(currentTime).then(() => {
-    player.play();
-  });
+player.on('play', function() {
+  console.log('Відео почало відтворюватися!');
 });
 
+function updateTime() {
+  player.getCurrentTime()
+    .then(function(currentTime) {
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    })
+    .catch(function(error) {
+      console.log('Помилка отримання поточного часу відтворення:', error);
+    });
+};
+
+const throttledUpdateTime = throttle(updateTime, 1000);
+
+const savedTimeVideo = localStorage.getItem('videoplayer-current-time');
+if (savedTimeVideo) {
+  player.setCurrentTime(savedTimeVideo)
+    .then(function (seconds) {
+      console.log('Встановлено позицію відтворення на ', seconds, 'секунд');
+    })
+    .catch(function (error) {
+       console.log('Помилка встановлення часу відтворення:', error);
+    });
+};
+
+player.on('timeupdate', throttledUpdateTime);
